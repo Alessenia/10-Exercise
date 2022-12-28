1d979e1b749f820d79cbbb2f46fc691aec13ae19
Alessenia <92642163+Alessenia@users.noreply.github.com>
2022-12-28 19:27:02 +0200

    Add files via upload

---
+++ my-c-compiler.sh
@@ -0,0 +1,39 @@
+#! /usr/bin/bash
+
+touch results.txt
+
+echo "To create a new C file, type the file name :"
+read filename
+
+echo "The c file has been created"
+
+echo "Do you want to compile your file?"
+echo "write y for yes or n for no "
+read choice
+
+if [ "$choice" = "y" ];
+then
+echo "compiling $filename... "
+sleep 2
+
+    echo "Compiling the program" | cat >> results.txt
+
+if  gcc $filename -o filename;
+    then
+        chmod +x ./$filename
+        echo "Succesful compilation" | cat >> results.txt
+        echo "
+        " | cat >> results.txt
+        echo "Your program results :" | cat >> results.txt
+        echo "
+        " | cat >> results.txt
+
+          else
+       echo "Your program hasn't been compiled" | cat >> results.txt
+
+    fi
+
+elif [ "$choice" = "n" ];
+then
+echo "The end"
+fi
